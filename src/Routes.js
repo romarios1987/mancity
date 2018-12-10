edfa68e8ade26f44636e93bee17315d7459e1f37
romarios1987 <romios047@gmail.com>
2018-12-10 23:53:18 +0200

    added resources, Header

---
+++ src/Routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from "./components/hoc/Layout";
+
+
+
+const Routes = (props) => {
+    return (
+        <div>
+           <Layout/>
+        </div>
+    );
+};
+
+export default Routes;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {BrowserRouter} from 'react-router-dom';
+import Routes from "./Routes";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const App = () =>(
+    <BrowserRouter>
+        <Routes/>
+    </BrowserRouter>
+);
+
+ReactDOM.render(<App/>, document.getElementById('root'));
