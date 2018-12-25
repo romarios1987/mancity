eaae862d21e10df563249e8d0cc4cb6e976be98d
romarios1987 <romios047@gmail.com>
2018-12-25 20:33:03 +0200

    Home: Meet the players component

---
+++ src/components/home/meetPlayers/HomeCards.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react';
+import {easePolyOut} from 'd3-ease';
+import Animate from 'react-move/Animate';
+import Otamendi from '../../../resources/images/players/Otamendi.png';
+import PlayerCard from "../../ui/playerCard";
+
+
+class HomeCards extends Component {
+
+    state = {
+        show: this.props.show,
+        cards: [
+            {
+                bottom: 90,
+                left: 300
+            },
+            {
+                bottom: 60,
+                left: 200
+            },
+            {
+                bottom: 30,
+                left: 100
+            },
+            {
+                bottom: 0,
+                left: 0
+            }
+        ]
+    };
+
+    showAnimateCards = () => (
+        this.state.cards.map((card, i) => (
+            <Animate
+                key={i}
+                show={this.props.show}
+                start={{
+                    bottom: 0,
+                    left: 0
+                }}
+                enter={{
+                    left: [card.left],
+                    bottom: [card.bottom],
+                    timing: {duration: 500, ease: easePolyOut}
+                }}
+            >
+                {({left, bottom}) => {
+                    return (
+                        <div style={{position: 'absolute', left, bottom}}>
+                            <PlayerCard
+                                number="30"
+                                name="Nicolas"
+                                lastname="Otamendi"
+                                bck={Otamendi}
+                            />
+                        </div>
+                    )
+                }}
+            </Animate>
+        ))
+    );
+
+    render() {
+        return (
+            <div>
+                {this.showAnimateCards()}
+            </div>
+        );
+    }
+}
+
+export default HomeCards;
